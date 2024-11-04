56200269e88cac5d9a5dc6da04e6e983d70db16a
Michael Campos <mjcampos@users.noreply.github.com>
2024-11-04 11:19:41 +0100

    Update octopus.gd
    Converted _process() to _physics_process() and put the code of toggle_collision() directly into it

--- Scenes/Enemy/Octopus/octopus.gd
+++ Scenes/Enemy/Octopus/octopus.gd
@@ -12,38 +12,41 @@ extends Enemy
 @onready var right_foot: CollisionShape2D = $RightFoot
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	toggle_collisions()
-
-func toggle_collisions():
+func _physics_process(_delta):
 	match animated_sprite_2d.frame:
 		0:
-			# Thighs
-			left_thigh.shape.set_size(Vector2(9.0, 6.0))
-			left_thigh.position = Vector2(-13.5, 9.0)
-			
-			right_thigh.shape.set_size(Vector2(9.0, 6.0))
-			right_thigh.position = Vector2(13.5, 9.0)
-			
-			# Legs
-			left_leg.position = Vector2(-18.0, 15.0)
-			right_leg.position = Vector2(18.0, 15.0)
-			
-			# Feet
-			left_foot.position = Vector2(-30.0, 21.0)
-			right_foot.position = Vector2(30.0, 21.0)
+			frame_starting_position()
 		1:
-			# Thighs
-			left_thigh.shape.set_size(Vector2(15.0, 6.0))
-			left_thigh.position = Vector2(-16.5, 9.0)
-			
-			right_thigh.shape.set_size(Vector2(15.0, 6.0))
-			right_thigh.position = Vector2(16.5, 9.0)
-			
-			# Legs
-			left_leg.position = Vector2(-24.0, 15.0)
-			right_leg.position = Vector2(24.0, 15.0)
-			
-			# Feet
-			left_foot.position = Vector2(-18.0, 21.0)
-			right_foot.position = Vector2(18.0, 21.0)
+			frame_end_position()
+
+func frame_starting_position():
+	# Thighs
+	left_thigh.shape.set_size(Vector2(9.0, 6.0))
+	left_thigh.position = Vector2(-13.5, 9.0)
+	
+	right_thigh.shape.set_size(Vector2(9.0, 6.0))
+	right_thigh.position = Vector2(13.5, 9.0)
+	
+	# Legs
+	left_leg.position = Vector2(-18.0, 15.0)
+	right_leg.position = Vector2(18.0, 15.0)
+	
+	# Feet
+	left_foot.position = Vector2(-30.0, 21.0)
+	right_foot.position = Vector2(30.0, 21.0)
+	
+func frame_end_position():
+	# Thighs
+	left_thigh.shape.set_size(Vector2(15.0, 6.0))
+	left_thigh.position = Vector2(-16.5, 9.0)
+	
+	right_thigh.shape.set_size(Vector2(15.0, 6.0))
+	right_thigh.position = Vector2(16.5, 9.0)
+	
+	# Legs
+	left_leg.position = Vector2(-24.0, 15.0)
+	right_leg.position = Vector2(24.0, 15.0)
+	
+	# Feet
+	left_foot.position = Vector2(-18.0, 21.0)
+	right_foot.position = Vector2(18.0, 21.0)
