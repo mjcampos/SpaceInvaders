7865f663a057b7bcf1e3f042a709470a11394892
Michael Campos <mjcampos@users.noreply.github.com>
2024-11-21 17:33:43 -0800

    Set points

--- Scenes/Enemy/UFO/ufo.gd
+++ Scenes/Enemy/UFO/ufo.gd
@@ -1,4 +1,12 @@
 extends Enemy
 
+var points = [50, 100, 150, 200, 300]
+
+func _ready():
+	_points = points[randi() % points.size()]  # Set initial random point
+
 func _on_area_entered(area: Area2D):
 	handle_laser_detection()
+
+func _on_timer_timeout():
+	_points = points[randi() % points.size()]
